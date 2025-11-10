ad1eb140865d180e63f0e9a222c41bfcf80f30ca
Felipe Corrêa <felipemessiasalvescorrea010@gmail.com>
2025-11-09 21:32:23 -0300

    finalizei o projeto starbucks

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let imagem = document.querySelector(".imagem-copo")
+let fundo = document.querySelector(".circulo")
 
 function trocaImagem(address) {
   imagem.src = address
 }
+
+function trocaFundo(cor) {
+  fundo.style.background = (cor)
+}
+
